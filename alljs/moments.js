8fdfbec9ae7c2fff2a6fce947a25cb5f90125f9e
Sujan Rana <khembikram1@gmail.com>
2023-07-05 13:59:55 +0545

    wed 1

---
+++ alljs/moments.js
@@ -0,0 +1,23 @@
+window.addEventListener("scroll", function () {
+    var navbar = document.querySelector(".nav");
+    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  
+    if (scrollPosition > 0) {
+      navbar.classList.add("navbar-scrolled");
+    } else {
+      navbar.classList.remove("navbar-scrolled");
+    }
+  });
+  
+
+  // hamburger starts 
+
+function burger(){
+  var x = document.getElementById('navmenu');
+  if(x.style.display === "block"){
+    x.style.display = "none" ;
+  }else{
+    x.style.display = "block";
+  }
+}
+  
