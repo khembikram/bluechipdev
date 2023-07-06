b32921ada8520923005e287565877811ee6a9e0a
Tauwa980 <tauwadigital@gmail.com>
2023-07-06 11:38:37 +0545

    new commit

--- alljs/home.js
+++ alljs/home.js
@@ -12,8 +12,8 @@ window.addEventListener("scroll", function () {
 // carousel
 
 document.addEventListener("DOMContentLoaded", function () {
-  var carouselSlide = document.querySelector(".carousel-slide");
-  var carouselSlides = document.querySelectorAll(".carousel-slide .slide");
+  var carouselSlide = document.querySelector(".testislide-slide");
+  var carouselSlides = document.querySelectorAll(".testislide-slide .slide");
   var bullets = document.querySelectorAll(".bullet");
 
   var currentIndex = 0;
@@ -41,14 +41,14 @@ document.addEventListener("DOMContentLoaded", function () {
   function goToPrevSlide() {
     currentIndex--;
     if (currentIndex < 0) {
-      currentIndex = carouselSlides.length - 1;
+      currentIndex = testislide.length - 1;
     }
     showSlide(currentIndex);
   }
 
   function goToNextSlide() {
     currentIndex++;
-    if (currentIndex >= carouselSlides.length) {
+    if (currentIndex >= testislide.length) {
       currentIndex = 0;
     }
     showSlide(currentIndex);
@@ -77,33 +77,3 @@ function burger() {
 }
 
 // hamburger ends
-
-// Get the desired element
-const myElement = document.getElementById('myElement');
-
-// Function to check if element is in view
-function isInView(element) {
-  const rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
-// Function to handle scroll event
-function handleScroll() {
-  if (isInView(myElement)) {
-    // Element is in view, add transition class after 5 seconds
-    setTimeout(() => {
-      myElement.classList.add('transition-class');
-    }, 5000);
-  } else {
-    // Element is out of view, remove transition class
-    myElement.classList.remove('transition-class');
-  }
-}
-
-// Add scroll event listener
-window.addEventListener('scroll', handleScroll);
