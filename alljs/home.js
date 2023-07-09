a4b7536046f55379b9f99734b4f4b38f1d8ae380
Tauwa980 <tauwadigital@gmail.com>
2023-07-09 16:19:05 +0545

    animatino added

--- alljs/home.js
+++ alljs/home.js
@@ -130,3 +130,18 @@ window.addEventListener("scroll", function () {
 });
 // scroll animation ends
 
+
+// overall animation
+
+const observer = new IntersectionObserver((entries) =>{
+  entries.forEach((entry) => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
